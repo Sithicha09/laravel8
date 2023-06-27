b895a54c8d59e89838733f5b866662545715bef1
Sithicha09 <sithicha.ban@vru.ac.th>
2023-06-27 16:04:26 +0700

    T 27/06/23

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/homepage", function () {
+    return "<h1>This is home page</h1>";
+});
+
+Route::get("/blog/{id}", function ($id) {
+    return "<h1>This is blog page : {$id} </h1>";
+});
+
+Route::get("/hello", function () {
+    return view("hello");
+});
+
+Route::get('/greeting', function () {
+    $name = 'James';
+    $last_name = 'Mars';
+    return view('greeting', compact('name', 'last_name'));
+});
