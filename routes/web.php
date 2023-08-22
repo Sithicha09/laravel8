937efd22d72acbd8b06ac8932b288137467244ca
Sithicha09 <sithicha.ban@vru.ac.th>
2023-08-22 16:03:28 +0700

    week9

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,13 @@ use App\Http\Controllers\MyProfileController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StaffController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+use App\Http\Controllers\ProfileController;  //เขียนเพิ่ม
+use App\Http\Controllers\UserController;  //เขียนเพิ่ม
+use App\Http\Controllers\VehicleController;  //เขียนเพิ่ม
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -144,3 +151,14 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
+
+//Route::resource('post', 'PostController');
+Route::resource('post', PostController::class);
+
+// Route::resource('profile', 'ProfileController');
+// Route::resource('user', 'UserController');
+// Route::resource('vehicle', 'VehicleController');
+
+Route::resource('profile', ProfileController::class);
+Route::resource('user', UserController::class);
+Route::resource('vehicle', VehicleController::class);
